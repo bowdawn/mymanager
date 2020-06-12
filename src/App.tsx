9ea46964451feb9e2882173ef814d7463064b23a
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-06-12 19:19:08 +0900

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
+import React, { FC, useState, useLayoutEffect } from 'react';
+import { Button, Row, Col } from 'antd';
+import 'src/App.less';
+import Header from 'src/components/header';
 
-import React, { FC } from 'react';
-import { Button } from 'antd';
-import './App.less';
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
 
-const App: FC = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+const App: FC = () => {
+  const [width, height] = useWindowSize();
+  const maxWidth = 500;
+  const maxHeight = 900;
+
+  return (
+    <div
+      className='App'
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          border: 'solid 1px lightgreen',
+          borderRadius: 10,
+          width: width > maxWidth ? maxWidth : width,
+          height: height > maxHeight ? maxHeight : height,
+        }}
+      >
+        <Header></Header>
+      </div>
+    </div>
+  );
+};
 
 export default App;
