3cbb4f28e56243b94062eaa986ab8e0070fa4bcf
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-06-22 19:44:10 +0900

    edit to navigation page

--- src/pages/dummy.tsx
+++ src/pages/dummy.tsx
@@ -11,10 +11,12 @@ const DummyPage: FC = (props: any) => {
           안녕하세요, 네이비게이션 페이지입니다, 다른 페이지로 이동하려면 링크를
           눌러주세요!
         </Title>
-        <Link href='/dummypage1'>페이지1 으로 이동 (진행중)</Link>
-        <Link href='/dummypage2'>페이지2 으로 이동 (진행중)</Link>
-        <Link href='/dummypage3'>페이지3 으로 이동 (진행중)</Link>
-        <Link href='/dummypage4'>페이지4 으로 이동 (진행중)</Link>
+        <Link href='/dummypage1' style={{ color: 'green' }}>
+          페이지1 으로 이동 (디자인 완료)
+        </Link>
+        <Link href='/dummypage2'>페이지2 으로 이동 (디자인 진행중)</Link>
+        <Link href='/dummypage3'>페이지3 으로 이동 (디자인 진행중)</Link>
+        <Link href='/dummypage4'>페이지4 으로 이동 (디자인 진행중)</Link>
       </Space>
     </div>
   );
