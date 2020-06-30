d5090f2b3a40ff5899efba1e3a58bb36784f4573
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-06-30 18:50:07 +0900

    go back function added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,27 @@
-import React, { FC, ReactHTMLElement, ReactChild } from 'react';
-import { message, PageHeader } from 'antd';
+import React, { FC, ReactChild } from 'react';
+import { PageHeader } from 'antd';
 
 import { ReactComponent as ArrowLeft } from 'src/assets/icons/header-arrow-left.svg';
 import Icon from '@ant-design/icons';
 import 'src/assets/components/header.less';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 interface Props {
   title: string;
-  extra?: ReactChild;
+  extra?: ReactChild | string;
   backgroundColor?: true;
 }
 
 const Header: FC<Props> = ({ title, extra, backgroundColor }) => {
+  const history = useHistory();
   return (
     <div {...(backgroundColor ? { className: 'my-header-background' } : {})}>
       <PageHeader
         className='my-header'
         backIcon={
-          <Link to=''>
-            <Icon className='arrow-left' component={() => <ArrowLeft />} />
-          </Link>
+          <Icon className='arrow-left' component={() => <ArrowLeft />} />
         }
-        onBack={() => message.info('header back: to be implemented')}
+        onBack={() => history.goBack()}
         title={null}
         extra={extra ? <div>{extra}</div> : <div></div>}
       >
